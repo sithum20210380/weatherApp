9c84a5a02018c98d8d92829a1929217d76d0de29
Sithum Raveesha <85311920+sithum20210380@users.noreply.github.com>
2024-01-01 00:32:16 +0530

    added weather day views

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -18,6 +18,53 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("76°")
+                        .font(.system(size: 70,weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 20){
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "cloud.sun.rain.fill",
+                                   temperature: 88)
+                    
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "smoke.fill",
+                                   temperature: 55)
+                    
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "wind",
+                                   temperature: 60)
+                    
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.sun.bolt.fill",
+                                   temperature: 25)
+                    
+                }
+                Spacer()
+                
+                Button{
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                }
+                
                 Spacer()
             }
         }
@@ -26,4 +73,27 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 24, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+    }
 }
